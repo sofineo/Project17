5012c31468f46e9e78e13ffdd0ed9108e7fb55ba
Sofia Neo <106977790+sofineo@users.noreply.github.com>
2023-05-26 23:30:28 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,7 +15,7 @@ export function Header(){
     signOut()
   }
 
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}files/${user.avatar}` : avatarPlaceHolder
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceHolder
 
   return(
     <Container>
